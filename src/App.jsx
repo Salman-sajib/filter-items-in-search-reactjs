a82bff9b13fcec8764fde57d25aa9e7c37283018
Salman-sajib <sfsajib@gmail.com>
2024-01-28 14:52:06 +0600

    installed tailwind to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,37 @@
+/* eslint-disable react/jsx-key */
+import { useRef, useState } from 'react';
+
 export default function App() {
-  return <h1>Hello React</h1>;
+  const [items, setItems] = useState(['an item']);
+  const inputRef = useRef();
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    const value = inputRef.current.value;
+    if (value === '') return;
+    setItems((prev) => {
+      return [...prev, value];
+    });
+    inputRef.current.value = '';
+  }
+
+  return (
+    <div>
+      Searct:
+      <input type='search' />
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        New Item: <input ref={inputRef} type='text' />
+        <button type='submit'>Add</button>
+      </form>
+      <h3>Items:</h3>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
